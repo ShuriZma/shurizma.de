e2130ffac6aca60f6c4042c93dfa0b7d16baecd1
shurizma <gggezggg@gmail.com>
2019-08-10 19:57:29 +0200

    l4d2 mods update

--- php/mods.page.php
+++ php/mods.page.php
@@ -512,9 +512,97 @@
         </a>
         <div id="l4d2" style="display:none">
             <ul>
-                <a class="red-link" href="mods/l4d2/Left4Dead2_mods.zip" download><?php echo $lang['mods-all'] ?></a>
-                <p class="hinweis"><?php echo $lang['mods-l4d2-all-size'] ?></p>
+                <a class="red-link" href="https://steamcommunity.com/id/shurizma/myworkshopfiles/?section=collections&appid=550"><?php echo $lang['mods-all'] ?></a>
                 <p><?php echo $lang['mods-l4d2-p1'] ?></p>
+                <p><?php echo $lang['mods-l4d2-p2'] ?></p>
+
+                <a href="javascript:toggle('l4d2-chars-1')">
+                    <h4><?php echo $lang['mods-l4d2-chars-1'] ?><span class="red-link">*</span></h4>
+
+                </a>
+                <div id="l4d2-chars-1" style="display:none">
+                    <ul>
+                        <a class="red-link" href="https://steamcommunity.com/sharedfiles/filedetails/?id=1810790067"><?php echo $lang['mods-l4d2-collection'] ?></a>
+                        <p><?php echo $lang['mods-l4d2-chars-1-p1'] ?></p>
+                        <p><?php echo $lang['mods-l4d2-chars-1-p2'] ?></p>
+                        <p><?php echo $lang['mods-l4d2-chars-change'] ?></p>
+                    </ul>
+                </div>
+
+                <a href="javascript:toggle('l4d2-chars-2')">
+                    <h4><?php echo $lang['mods-l4d2-chars-2'] ?><span class="red-link">*</span></h4>
+
+                </a>
+                <div id="l4d2-chars-2" style="display:none">
+                    <ul>
+                        <a class="red-link" href="https://steamcommunity.com/sharedfiles/filedetails/?id=1810710640"><?php echo $lang['mods-l4d2-collection'] ?></a>
+                        <p><?php echo $lang['mods-l4d2-chars-2-p1'] ?></p>
+                        <p><?php echo $lang['mods-l4d2-chars-2-p2'] ?></p>
+                        <p><?php echo $lang['mods-l4d2-chars-change'] ?></p>
+                    </ul>
+                </div>
+
+                <a href="javascript:toggle('l4d2-melee')">
+                    <h4><?php echo $lang['mods-l4d2-melee'] ?><span class="red-link">*</span></h4>
+
+                </a>
+                <div id="l4d2-melee" style="display:none">
+                    <ul>
+                        <a class="red-link" href="https://steamcommunity.com/sharedfiles/filedetails/?id=1810712933"><?php echo $lang['mods-l4d2-collection'] ?></a>
+                        <p><?php echo $lang['mods-l4d2-melee-p1'] ?></p>
+                        <p><?php echo $lang['mods-l4d2-melee-p2'] ?></p>
+                    </ul>
+                </div>
+
+                <a href="javascript:toggle('l4d2-weapons-1')">
+                    <h4><?php echo $lang['mods-l4d2-weapons-1'] ?><span class="red-link">*</span></h4>
+
+                </a>
+                <div id="l4d2-weapons-1" style="display:none">
+                    <ul>
+                        <a class="red-link" href="https://steamcommunity.com/sharedfiles/filedetails/?id=1124637766"><?php echo $lang['mods-l4d2-collection'] ?></a>
+                        <p><?php echo $lang['mods-l4d2-weapons-1-p1'] ?></p>
+                        <p><?php echo $lang['mods-l4d2-weapons-1-p2'] ?></p>
+                        <p><?php echo $lang['mods-l4d2-weapons-change'] ?></p>
+                    </ul>
+                </div>
+
+                <a href="javascript:toggle('l4d2-weapons-2')">
+                    <h4><?php echo $lang['mods-l4d2-weapons-2'] ?><span class="red-link">*</span></h4>
+
+                </a>
+                <div id="l4d2-weapons-2" style="display:none">
+                    <ul>
+                        <a class="red-link" href="https://steamcommunity.com/sharedfiles/filedetails/?id=1124632604"><?php echo $lang['mods-l4d2-collection'] ?></a>
+                        <p><?php echo $lang['mods-l4d2-weapons-2-p1'] ?></p>
+                        <p><?php echo $lang['mods-l4d2-weapons-2-p2'] ?></p>
+                        <p><?php echo $lang['mods-l4d2-weapons-change'] ?></p>
+                    </ul>
+                </div>
+
+                <a href="javascript:toggle('l4d2-infected')">
+                    <h4><?php echo $lang['mods-l4d2-infected'] ?><span class="red-link">*</span></h4>
+
+                </a>
+                <div id="l4d2-infected" style="display:none">
+                    <ul>
+                        <a class="red-link" href="https://steamcommunity.com/sharedfiles/filedetails/?id=1810729694"><?php echo $lang['mods-l4d2-collection'] ?></a>
+                        <p><?php echo $lang['mods-l4d2-infected-p1'] ?></p>
+                        <p><?php echo $lang['mods-l4d2-infected-p2'] ?></p>
+                    </ul>
+                </div>
+
+                <a href="javascript:toggle('l4d2-others')">
+                    <h4><?php echo $lang['mods-l4d2-others'] ?><span class="red-link">*</span></h4>
+
+                </a>
+                <div id="l4d2-others" style="display:none">
+                    <ul>
+                        <a class="red-link" href="https://steamcommunity.com/sharedfiles/filedetails/?id=1124632350"><?php echo $lang['mods-l4d2-collection'] ?></a>
+                        <p><?php echo $lang['mods-l4d2-others-p1'] ?></p>
+                        <p><?php echo $lang['mods-l4d2-others-p2'] ?></p>
+                    </ul>
+                </div>
             </ul>
         </div>
     </ul>
