276c722062c4cd78bfc9725f1454b56c1a1174ee
shurizma <gggezggg@gmail.com>
2019-10-21 13:53:02 +0200

    fix

--- php/home.page.php
+++ php/home.page.php
@@ -16,7 +16,7 @@
         <h3><?php echo $lang['home-übersicht'] ?></h3>
         <ul class="no-dots-ul">
             <li>
-                <h4><?php echo $lang['home-übersicht-home'] ?></h4>
+                <h4><a class="no-text-transform" href="?f=home&lang=<?php echo $_SESSION['lang'] ?>"><?php echo $lang['home-übersicht-home'] ?></a></h4>
                 <p><?php echo $lang['home-übersicht-home-p1'] ?></p>
             </li>
             <li>
@@ -24,28 +24,28 @@
                 <p><?php echo $lang['home-übersicht-info-p1'] ?></p>
                 <ul class="no-dots-ul">
                     <li>
-                        <h4><?php echo $lang['home-übersicht-info-featured'] ?></h4>
+                        <h4><a class="no-text-transform" href="?f=featured&lang=<?php echo $_SESSION['lang'] ?>"><?php echo $lang['home-übersicht-info-featured'] ?></a></h4>
                         <p><?php echo $lang['home-übersicht-info-featured-p1'] ?></p>
                     </li>
                     <li>
-                        <h4><?php echo $lang['home-übersicht-info-mods'] ?></h4>
+                        <h4><a class="no-text-transform" href="?f=mods&lang=<?php echo $_SESSION['lang'] ?> "><?php echo $lang['home-übersicht-info-mods'] ?></a></h4>
                         <p><?php echo $lang['home-übersicht-info-mods-p1'] ?></p>
                     </li>
                     <li>
-                        <h4><?php echo $lang['home-übersicht-info-partner'] ?></h4>
+                        <h4><a class="no-text-transform" href="?f=partner&lang=<?php echo $_SESSION['lang'] ?>"><?php echo $lang['home-übersicht-info-partner'] ?></a></h4>
                         <p><?php echo $lang['home-übersicht-info-partner-p1'] ?></p>
                         <p><?php echo $lang['home-übersicht-info-partner-p2'] ?></p>
                     </li>
                     <li>
-                        <h4><?php echo $lang['home-übersicht-info-specs'] ?></h4>
+                        <h4><a class="no-text-transform" href="?f=specs&lang=<?php echo $_SESSION['lang'] ?> "><?php echo $lang['home-übersicht-info-specs'] ?></a></h4>
                         <p><?php echo $lang['home-übersicht-info-specs-p1'] ?></p>
                     </li>
                     <li>
-                        <h4><?php echo $lang['home-übersicht-info-streaming'] ?></h4>
+                        <h4><a class="no-text-transform" href="?f=streaming-info&lang=<?php echo $_SESSION['lang'] ?>"><?php echo $lang['home-übersicht-info-streaming'] ?></a></h4>
                         <p><?php echo $lang['home-übersicht-info-streaming-p1'] ?></p>
                     </li>
                     <li>
-                        <h4><?php echo $lang['home-übersicht-info-whois'] ?></h4>
+                        <h4><a class="no-text-transform" href="?f=wer-ist-shurizma&lang=<?php echo $_SESSION['lang'] ?> "><?php echo $lang['home-übersicht-info-whois'] ?></a></h4>
                         <p><?php echo $lang['home-übersicht-info-whois-p1'] ?></p>
                     </li>
                 </ul>
@@ -55,7 +55,7 @@
                 <p><?php echo $lang['home-übersicht-spiele-p1'] ?></p>
                 <ul class="no-dots-ul">
                     <li>
-                        <h4><?php echo $lang['home-übersicht-spiele-payday2'] ?></h4>
+                        <h4><a class="no-text-transform" href="?f=payday&lang=<?php echo $_SESSION['lang'] ?>"><?php echo $lang['home-übersicht-spiele-payday2'] ?></a></h4>
                         <p><?php echo $lang['home-übersicht-spiele-payday2-p1'] ?></p>
                     </li>
                 </ul>
@@ -65,14 +65,14 @@
                 <p><?php echo $lang['home-übersicht-galerie-p1'] ?></p>
                 <ul class="no-dots-ul">
                     <li>
-                        <h4><?php echo $lang['home-übersicht-galerie-gta'] ?></h4>
+                        <h4><a class="no-text-transform" href="?f=gta-gallery&lang=<?php echo $_SESSION['lang'] ?>"><?php echo $lang['home-übersicht-galerie-gta'] ?></a></h4>
                         <p><?php echo $lang['home-übersicht-galerie-gta-p1'] ?></p>
                         <p><?php echo $lang['home-übersicht-galerie-gta-p2'] ?></p>
                     </li>
                 </ul>
             </li>
             <li>
-                <h4><?php echo $lang['home-übersicht-forum'] ?></h4>
+                <h4><a href="https://forum.shurizma.de"><?php echo $lang['home-übersicht-forum'] ?></a></h4>
                 <p><?php echo $lang['home-übersicht-forum-p1'] ?></p>
             </li>
             <li>
